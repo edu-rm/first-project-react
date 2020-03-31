aa9b77146719c5357dfc70d66de2f78986424485
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-31 13:49:39 -0300

    button animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
@@ -9,6 +9,7 @@ export default class Main extends Component {
   state = {
     newRepo: '',
     repositories: [],
+    loading: false,
   };
 
   handleInputChange = (e) => {
@@ -17,6 +18,8 @@ export default class Main extends Component {
 
   handleSubmit = async (e) => {
     e.preventDefault();
+
+    this.setState({ loading: true });
     const { newRepo, repositories } = this.state;
 
     const response = await api.get(`/repos/${newRepo}`);
@@ -29,12 +32,13 @@ export default class Main extends Component {
     this.setState({
       repositories: [...repositories, data],
       newRepo: '',
+      loading: false,
     });
 
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, loading } = this.state;
 
     return (
       <Container>
@@ -50,8 +54,13 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton>
-            <FaPlus color="#fff" size={14} />
+          <SubmitButton loading={loading}>
+            { loading ? (
+              <FaSpinner color="#fff" size={14}/>
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+
           </SubmitButton>
         </Form>
       </Container>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -34,9 +34,19 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-})`
+  disabled: props.loading,
+}))`
   background: #0077b5;
   border: none;
   padding: 0 16px;
@@ -46,4 +56,15 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   align-items: center;
   justify-content: center;
+
+  &[disabled]{
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${props => props.loading && css`
+    svg {
+      animation: ${rotate} 2s linear infinite;
+    }
+  `}
 `;
