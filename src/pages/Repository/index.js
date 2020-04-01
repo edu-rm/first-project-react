9127b1262b2d7341fa0dbd2654e2244026b14662
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-01 10:21:39 -0300

    get more repository data form github api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repository :{},
+    issues:[],
+    loading: true,
+  };
+
+
+  async componentDidMount(){
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues?state=open&per_page=5`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        }
+      }),
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data,
+    });
+
+    console.log(repository);
+    console.log(issues);
+
+
+  }
+
+  render(){
+    return <h1>Repository: </h1>;
+  }
 }
